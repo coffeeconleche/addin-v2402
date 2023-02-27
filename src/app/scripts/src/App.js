ebf06d441fab1f5b1d39ff2f9156f11c4ee58603
coffeeconleche <77335621+coffeeconleche@users.noreply.github.com>
2023-02-27 01:13:33 -0500

    Replica CL SF

--- src/app/scripts/src/App.js
+++ src/app/scripts/src/App.js
@@ -1,5 +1,29 @@
 import React from "react";
 import Switch from "@mui/material/Switch";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+import Radio from "@mui/material/Radio";
+import RadioGroup from "@mui/material/RadioGroup";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import FormControl from "@mui/material/FormControl";
+import FormLabel from "@mui/material/FormLabel";
+import { downloadBlob } from "./descargar";
+function createData(name, calories, fat, carbs, protein) {
+  return { name, calories, fat, carbs, protein };
+}
+
+const rows = [
+  createData("Tarjeta de propiedad"),
+  createData("SOAT"),
+  createData("Llave de contacto"),
+  createData("Llanta de repuesto"),
+  createData("Llave de ruedas"),
+];
 
 const App = ({ device, session, api }) => {
   const label = { inputProps: { "aria-label": "Switch demo" } };
@@ -43,8 +67,22 @@ const App = ({ device, session, api }) => {
       }
     );
   };
+  const descargarCL = () => {
+    try {
+      downloadBlob(
+        "https://test-publico.s3.amazonaws.com/Check+list+camionetas.pdf",
+        "Checklist_SF"
+      );
+    } catch (error) {
+      console.log("Error al descargar CL: ", error);
+    }
+  };
+  const Odometro = () => <>El odómetro es {odometroDiv} km</>;
+  const [selectedValue, setSelectedValue] = React.useState("");
 
-  const Odometro = () => <h1>El odómetro es {odometroDiv}</h1>;
+  const handleChange = (event) => {
+    setSelectedValue(event.target.value);
+  };
   return (
     <div>
       <div>
@@ -57,74 +95,96 @@ const App = ({ device, session, api }) => {
         <Switch {...label} disabled />
       </div>
       <h1>Diego - Primer addin de Geotab Drive!</h1>
-      Test cambio build!
-      <h1>Bienvenido {session.userName}!</h1>
-      <h1>El vehículo que seleccionaste es {device.name}</h1>
-      Prueba scroll
+      Aquí se debería mostrar el usuario: <br />
+      {session.userName} <br />
       <br />
-      Prueba scroll
+      Aquí se debería mostrar el vehículo asignado: <br />
+      {device.name} <br />
       <br />
-      Prueba scroll
-      <br />
-      Prueba scroll
-      <br />
-      <div>
-        <Switch {...label} defaultChecked />
-      </div>
-      Prueba scroll
-      <br />
-      Prueba scroll
-      <br />
-      Prueba scroll
-      <br />
-      Prueba scroll
-      <br />
-      Prueba scroll
-      <br />
-      Prueba scroll
-      <br />
-      Prueba scroll
-      <br />
-      Prueba scroll
-      <br />
-      Prueba scroll
-      <br />
-      Prueba scroll
-      <br />
-      Prueba scroll
-      <br />
-      Prueba scroll
-      <br />
-      Prueba scroll
-      <br />
-      Prueba scroll
-      <br />
-      Prueba scroll
-      <br />
-      Prueba scroll
-      <br />
-      Prueba scroll
-      <br />
-      Prueba scroll
-      <br />
-      Prueba scroll
-      <br />
-      Prueba scroll
-      <br />
-      Prueba scroll
-      <br />
-      Prueba scroll
-      <br />
-      Prueba scroll
+      Este botón debería mostrar el odómetro actual del vehículo asignado:
       <br />
       <button
         className="boton-odometro"
         type="button"
         onClick={() => obtenerOdometro({ device })}
       >
-        Consultar odómetro actual de {device.name}
+        Click para obtener odómetro de {device.name}
       </button>
-      <div>{openDiv ? <Odometro /> : null}</div>
+      <div>{openDiv ? <Odometro /> : null}</div> <br />
+      <h2>Tratando de replicar checklist de San Fernando</h2>
+      Aquí debería poder ver el logo de SF:
+      <br />
+      <center>
+        <img
+          src="https://play-lh.googleusercontent.com/f35RMXXJjKSlUptfB6DZ9tIqHxEVIS-cszNfZ5fvjHPxnrbgg9hr-KBdIEiUcz1Ba3M"
+          alt="Logo San Fernando"
+          width="50%"
+        ></img>
+      </center>
+      <br />
+      Aquí debería poder seleccionar SOLO UNA opción de las 3 disponibles:
+      <br />
+      <br />
+      <TableContainer component={Paper}>
+        <Table aria-label="simple table" style={{ width: "100%" }}>
+          <TableHead>
+            <TableRow>
+              <TableCell colSpan={4}>COMPONENTES EXTERNOS </TableCell>{" "}
+            </TableRow>
+
+            <TableRow>
+              <TableCell align="left">Requisito</TableCell>
+
+              <TableCell align="center">Bueno</TableCell>
+              <TableCell align="center">Regular</TableCell>
+              <TableCell align="center">Malo</TableCell>
+
+              {/* <TableCell align="left">Regular</TableCell>
+              <TableCell align="left">Malo</TableCell> */}
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {rows.map((row) => (
+              <TableRow
+                key={row.name}
+                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+              >
+                <TableCell>{row.name}</TableCell>
+                <TableCell align="center" colSpan={3}>
+                  {/* El colSpan es para indicar cuantas columnas abarca */}
+                  <FormControl hiddenLabel={true}>
+                    <RadioGroup row align="center">
+                      <FormControlLabel
+                        value="bueno"
+                        control={<Radio />}
+                        align="center"
+                      />
+
+                      <FormControlLabel
+                        value="regular"
+                        control={<Radio />}
+                        align="center"
+                      />
+
+                      <FormControlLabel
+                        value="malo"
+                        control={<Radio />}
+                        align="center"
+                      />
+                    </RadioGroup>
+                  </FormControl>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+      <br />
+      Aquí debería poder descargar el PDF del checklist original de SF:
+      <br />
+      <a href="Checklist_SF.pdf" download>
+        <button>Descargar CL de SF</button>
+      </a>
     </div>
   );
 };
